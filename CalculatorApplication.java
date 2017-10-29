ae727713377cdedb197aa4c62b2817c7f98bc4c3
antonioguadarrama <antonioguadarrama_@live.com>
2017-10-29 12:09:58 -0700

    Newest version of CalculatorApplication (10.29.17)
    Added most of the user input checks. Also have added a few methods for the calculations.

--- CalculatorApplication.java
+++ CalculatorApplication.java
@@ -42,7 +42,7 @@ public class CalculatorApplication {
                 // if is not a valid user input, restart the loop (restart program)
                 continue;
             }
-            
+                    
             output = computeEquation(userInput);
             
         }
@@ -51,12 +51,15 @@ public class CalculatorApplication {
     public static boolean checkUserInput(String userInput) {
         if(userInput.isEmpty()) {
             // bad user input (no input) return error and ask for new input
-            System.out.println("Bad user input\n");
+            System.out.println("Error: Bad user input\n");
             return false;
         } else if(userInput.equals("q")) {
             // end the program
             System.out.println("---Program will now exit---");
             System.exit(0);
+        } else if(userInput.length() > 50) {
+            System.out.println("Error: Input too long\n");
+            return false;
         } else if(checkParenthesis(userInput) == false) {
             return false;
         }
@@ -64,14 +67,100 @@ public class CalculatorApplication {
     }
     
     public static boolean checkParenthesis(String userInput) {
-        // check if there are the same number of closed parenthesis as open parenthesis
+        // variables
+        //StringTokenizer token = new StringTokenizer(userInput, "^*/+-0123456789");
+        ArrayList<String> characters = new ArrayList<String>(Arrays.asList(userInput.split("")));
+        
+        // add the tokens into the arraylist
+        //while(token.hasMoreTokens()) {
+        //    characters.add(token.nextToken());
+        //}
+        
+        int leftParenthesis = 0;
+        int rightParenthesis = 0;
+        
+        // count the number of left & right parenthesis (should be equal)
+        for (String character : characters) {
+            if(rightParenthesis > leftParenthesis) {
+                System.out.println("Error: closed parentheses before open parentheses\n");
+                return false;
+            }
+            if (character.equals("(")) {
+                leftParenthesis++;
+            } else if (character.equals(")")) {
+                rightParenthesis++;
+            }
+        }
+        // if number of open and closed parenthesis are not equal, return false
+        if(leftParenthesis != rightParenthesis) {
+            System.out.println("Error: number of open and closed parentheses are not equal\n");
+            return false;
+        }   // check if there are the same number of closed parenthesis as open parenthesis
         return true;
-    }
-    
+    }    
     public static double computeEquation(String userInput) {
         double answer = 0.0;
+        int startIndex = 0;
+        int endIndex = 0;
+        String currentEquation = "";
+        boolean equationSolved = false;
         
+        userInput = "(" + userInput + ")";
+        
+        while(!equationSolved) {
+            ArrayList<String> characters = new ArrayList<String>(Arrays.asList(userInput.split("")));
+            startIndex = findStart(characters); // starting index is updated
+            System.out.println("Start index: " + startIndex);
+            endIndex = findEnd(characters, startIndex);
+            System.out.println("End index: " + endIndex);
+            // parse the substring between the start index and end index
+            currentEquation = userInput.substring(endIndex, endIndex);
+            // then solve
+            solveEquation(currentEquation);
+            // update userInput
+            userInput = userInput.substring(0, startIndex) + currentEquation
+                    + userInput.substring(endIndex, userInput.length());
+        }
         return answer;
     }
     
+    public static int findStart(ArrayList<String> characters) {
+        int level = 0;      // current level of parenthesis
+        int maxLevel = 0;   // max level of parenthesis found
+        int maxIndex = 0;   // index of max level parenthesis found
+        
+        // find the furthest set of parenthesis and start solving there
+        for (int i = 0; i < characters.size(); i++) {
+            if(characters.get(i).equals("(")) {
+                level++;
+            } else if(characters.get(i).equals(")")) {
+                level--;
+            }
+            if(level > maxLevel) {
+                maxLevel = level;
+                maxIndex = i;
+            }
+        }
+        
+        return maxIndex;
+    }
+    
+    public static int findEnd(ArrayList<String> characters, int startIndex) {
+        for(int i = startIndex; i < characters.size(); i++) {
+            if(characters.get(i).equals(")")) {
+                return i;
+            }
+        }
+        return characters.size(); // 
+    }
+    
+    public static double solveEquation(String equation) {
+        double returnValue = 0.0;
+        // remove parenthesis at beginning and end
+        equation = equation.substring(1, equation.length()-1);
+        
+        
+        
+        return returnValue;
+    }
 }
